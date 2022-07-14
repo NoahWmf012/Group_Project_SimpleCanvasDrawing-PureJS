7c39fec00155802bb2c03d43c1ea879fc046eeb9
alex <alex92335126@gmail.com>
2022-07-14 17:31:06 +0800

    added animation

---
+++ canvas/increase-size.js
@@ -0,0 +1,31 @@
+// $("#drawing-circle").on("click", function () {
+//   $(this).css("height", "90px");
+//   $(this).css("width", "90px");
+// });
+
+// $("button").on("click", function () {
+//   $(this).css({
+//     "border-style": "solid",
+//     "border-width": "2px",
+//     " border-color": "#000",
+//     "padding": "20px 20px",
+//     "text-align": "center",
+//     "cursor": "pointer",
+//     "background-color": "white",
+//     "border-radius": "15px",
+//   });
+// });
+// $("button").on("mouseover", function () {
+//     $(this).css({
+//         "border-width": "4px",
+//     });
+//   });
+
+// $("button").on("click", function () {
+//     $(this).css({
+//         "background-color": "white",
+//         "box-shadow": "0 5px white",
+//         "transform": "translateY(6px)",
+//     });
+// });
+
